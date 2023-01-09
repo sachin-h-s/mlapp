a01f40ea6e5a95e360ec1b7a7736b332adf5c08e
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-09 11:55:39 +0530

    Update appnew.py

--- appnew.py
+++ appnew.py
@@ -5,26 +5,26 @@ from sklearn.ensemble import RandomForestClassifier
 st.title('ML App')
 
 # Load the data
-@st.cache
-def load_data(file):
-    df = pd.read_csv(file)
-    return df
+uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
-# Split the data into features and target
-X = df.drop('target', axis=1)
-y = df['target']
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
 
-# Train a model
-model = RandomForestClassifier()
-model.fit(X, y)
+    # Split the data into features and target
+    X = df.drop('target', axis=1)
+    y = df['target']
 
-# Create a sidebar with a slider for selecting a sample
-sample_index = st.sidebar.slider('Select a sample', 0, len(df)-1)
+    # Train a model
+    model = RandomForestClassifier()
+    model.fit(X, y)
 
-# Display the selected sample
-st.subheader('Selected sample')
-st.write(X.iloc[sample_index, :])
+    # Create a sidebar with a slider for selecting a sample
+    sample_index = st.sidebar.slider('Select a sample', 0, len(df)-1)
 
-# Predict the target for the selected sample
-prediction = model.predict(X.iloc[sample_index, :].values.reshape(1, -1))
-st.write('Prediction:', prediction)
+    # Display the selected sample
+    st.subheader('Selected sample')
+    st.write(X.iloc[sample_index, :])
+
+    # Predict the target for the selected sample
+    prediction = model.predict(X.iloc[sample_index, :].values.reshape(1, -1))
+    st.write('Prediction:', prediction[0])
